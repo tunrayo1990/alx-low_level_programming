0b5b449699058db2b8311db4963968b400fc589f
tunrayo1990 <adejumobi.tunrayo@gmail.com>
2022-09-08 12:15:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * Description: main - Prints out string to stdou
+ * Return: 0 if success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzeel");
+	Return (0);
+}
